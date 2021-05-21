f5efe272ba079db9581bc2ef1e50251e712e5ec0
Miquiis <miquiisoficial@gmail.com>
2021-05-21 17:25:27 -0400

    Added add and remove arrows to checkout page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { connect } from 'react-redux'
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInPage from './pages/signin/signin.component'
+import SignInPage from './pages/signin/signin.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component'
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
@@ -47,6 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : <SignInPage />} />
         </Switch>
       </div>
@@ -54,8 +58,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser : user.currentUser
+const mapStateToProps = state => ({
+    currentUser : selectCurrentUser(state)
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -7,3 +7,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const clearItemFromCart = (cartItems, cartItemToRemove) => {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+}
+
+export const removeItemToCart = (cartItems, cartItemToRemove) => {
+    if (cartItemToRemove.quantity > 1)
+    {
+        return cartItems.map(cartItem => cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1} : cartItem)
+    }
+
+    return clearItemFromCart(cartItems, cartItemToRemove)
+}
