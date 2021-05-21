df9021ad58b599905919dd49212b68399d3be9b4
Miquiis <miquiisoficial@gmail.com>
2021-05-20 23:30:20 -0400

    Added Redux to project

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,7 +47,7 @@ class SignIn extends React.Component {
                     <FormInput name='password' label='Password' type='password' value={this.state.password} handleChange={this.handleChange} required></FormInput>
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
