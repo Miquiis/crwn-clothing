488acfa79c75964333ef904fe5368e700b086cfb
Miquiis <miquiisoficial@gmail.com>
2021-05-22 20:47:50 -0400

    Added stripe payments (front-end)

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Iu4MrECqff28p3xMA5AvCBmKMgKGAis4wqbsWBV4EN6uSu1FFVsFGwjeN9oJ2YRNv23Gqk1hm0U5wzq6oXUvaOj00pbWplWe7';
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
